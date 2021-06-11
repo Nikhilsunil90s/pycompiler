f9f32eafd3d967e2a478685748b3024c25b4ca23
nikhlsunil90s <nikhlsunil90s@gmail.com>
2021-06-11 14:54:43 +0530

    Prompt Fixes

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+app.use('/public', express.static('public'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req,res,next) => {
+    res.render('pages/home');
+})
+
+const port = process.env.PORT || 8100;
+
+
+app.listen(port, () => {
+    console.log('App is Running!')
+
+})
